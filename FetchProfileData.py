8cf9ea337fa85aee535b492e68dd0e5bab5a5e56
johnsoe <evan.johnson.r@gmail.com>
2016-03-11 23:55:36 -0800

    First commit, create a simple script to request single user data and print it out

---
+++ FetchProfileData.py
@@ -0,0 +1,27 @@
+from lxml import html
+import requests
+
+def printPlayerStats(player):
+    response = requests.get('http://www.hotslogs.com/API/Players/1/' + player)
+    print response.json()
+
+def getFullPageStats(playerId):
+    response = requests.get("http://www.hotslogs.com/Player/Profile?PlayerID=" + playerId);
+    return html.fromstring(response.content)
+
+def getHeroStats(pageContent):
+    pass
+    #TODO parse the page to find winrates, lvl, games played etc.
+
+def getMapStats(pageContent):
+	pass
+    #Todo parse the page to find all map winrates and games played
+
+def getWinratesAgainstHeroes(pageContent):
+	pass
+    #TODO parse the page to find winrates against other heroes
+
+
+printPlayerStats("ralphstew_1376")
+printPlayerStats("schweik_1154")
+#printPlayerStats("appleeater") Need battletag
