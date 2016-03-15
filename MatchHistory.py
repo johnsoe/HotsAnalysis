08b15665b8aa92b5ed79c27ea33795bc2fc86beb
schweikhardt <schw16@gmail.com>
2016-03-15 00:17:24 -0700

    Update MatchHistory.py

--- MatchHistory.py
+++ MatchHistory.py
@@ -1,38 +1,53 @@
+
+
 import requests
-from BeautifulSoup import BeautifulSoup
+from bs4 import BeautifulSoup
+import re
 import csv
 
 
-url = 'http://www.hotslogs.com/Player/MatchHistory?PlayerID=5434184'
+gamenumber=63328771
+
+url = 'http://www.hotslogs.com/Player/MatchSummaryAjax?ReplayID=' + str(gamenumber)
+#url = 'http://www.hotslogs.com/Player/MatchHistory?PlayerID=5434184'
 response = requests.get(url)
 html = response.content
-#print html
-#soup = BeautifulSoup(html)
-#print soup.prettify()
+soup = BeautifulSoup(html,"lxml")
 
-soup = BeautifulSoup(html)
-#table = soup.find('table', attrs={'id': 'DataTables_Table_0'})
+#f1=open('./testfile', 'w+')
+
+player=[];
 table = soup.find('table', attrs={'class': 'rgMasterTable'})
-#print table.prettify()
-list_of_rows = []
+#Find Player ID's
+for a in table.find_all('a'):
+    t= re.findall('(?<=Profile\?PlayerID\=)\d*',a['href'])
+    if t:
+        player.append(t[0])
 
+print player
+#Find Match Details
+
+list_of_rows = []
+ind=-1
 for row in table.findAll('tr'):
     list_of_cells = []
-    #print row.prettify()
+    print 'newline'
     for cell in row.findAll('td'):
-        #print cell.text.replace('<td style="display:none;">', '')
-        cell2=cell.text.replace('</td>', '')
-        text = cell2.replace('&nbsp;','')
+    	#Converts Soup format into text, converts to ascii, and remvoes nbsp
+    	#Non-ascii characters will retain their identification
+        text=cell.text.encode("ascii","xmlcharrefreplace").replace('&#160;','')
+        
         list_of_cells.append(text)
-    list_of_rows.append(list_of_cells)
-        #cell=cell.text.replace('&nbsp;','')
-    	#print "row"
-        #print cell.prettify()
-        #print cell.text
-print list_of_rows
-#print '<td style="display:none;">'
-
+    
+    if len(list_of_cells)>2:
+    	ind=ind+1
+        del list_of_cells[5:12]
+        del list_of_cells[2]
+        list_of_cells[0]=player[ind]
+        print list_of_cells
+        list_of_rows.append(list_of_cells)
+    #del list_of_cells[1]
 outfile = open("./output.csv", "wb")
 writer = csv.writer(outfile)
 writer.writerows(list_of_rows)
-
+#id="MatchSummary_h3MapName"
